e9f6f3cd645d452314b3c4f8b6e340f4c6c877e0
Michael Casadevall <michaelc@them.com>
2017-06-23 00:37:53 -0400

    Refactor export scan to break host exportation into it's own SQL file type
    Signed-off-by: Michael Casadevall <michaelc@them.com>

--- sql/functions/network_scan/R__export_scan.sql
+++ sql/functions/network_scan/R__export_scan.sql
@@ -4,163 +4,30 @@ CREATE OR REPLACE FUNCTION network_scan.export_scan(_scan_id bigint) RETURNS jso
 DECLARE
     scan_json json;
     scan_row network_scan.scans;
+    current_host_id bigint;
+    host_ids bigint[];
     host_json_array json[];
-    host_address network_scan.flattened_host_addresses;
-    hostname_row network_scan.flattened_host_hostnames;
-    port_row network_scan.flattened_host_ports;
-    service_row network_scan.flattened_port_services;
-    service_cpes_row network_scan.flattened_service_cpes;
-    osmatch_row network_scan.flattened_host_osmatches;
-    osclass_row network_scan.flattened_host_osmatches_osclasses;
-    osclass_cpe_row network_scan.flattened_osclass_cpes;
-    script_output_row network_scan.flattened_script_output;
 BEGIN
     -- This basically works in reverse from import_scan, though we have some views that help
     -- simplify returning the data out of the pipe
 
-    FOR host_address IN SELECT * FROM network_scan.flattened_host_addresses WHERE scan_id=_scan_id
+    -- First, grab the scan from the scan table, we'll need the info later, and this lets us
+    -- check that we don't have gibberish coming in
+    SELECT * INTO scan_row FROM network_scan.scans WHERE id=_scan_id;
+    IF NOT FOUND THEN
+        RAISE EXCEPTION 'Scan ID % not found', _scan_id;
+    END IF;
+
+    -- The host JSON generation was moved to it's own function
+    host_ids := array(SELECT id FROM network_scan.hosts WHERE scan_id=_scan_id);
+    FOREACH current_host_id IN ARRAY host_ids
     LOOP
-        DECLARE
-            hostname_json_array json[];
-            port_json_array json[];
-            osmatches_json_array json[];
-        BEGIN
-            FOR hostname_row IN SELECT * FROM network_scan.flattened_host_hostnames WHERE host_id=host_address.host_id
-            LOOP
-                hostname_json_array := array_append(
-                    hostname_json_array,
-                        json_build_object(
-                        'hostname', hostname_row.hostname,
-                        'type', hostname_row.type
-                    )
-                );
-            END LOOP; -- hostname loop
-
-            -- Grab the port data
-            FOR port_row IN SELECT * FROM network_scan.flattened_host_ports WHERE host_id=host_address.host_id
-            LOOP
-                DECLARE
-                    service_json json;
-                    script_output_array json[];
-                    cpes_array json[];
-                BEGIN
-                    -- If the port is not present (i.e., surface scans, simply exclude it)
-                    IF NOT FOUND THEN
-                        port_json_array := NULL;
-                        EXIT;
-                    END IF;
-
-                    -- Grab the service information and append it
-                    service_json := NULL;
-                    SELECT * INTO service_row FROM network_scan.flattened_port_services WHERE host_port_id=port_row.host_port_id;
-                    IF FOUND THEN
-                        -- Grabs the CPEs
-                        FOR service_cpes_row IN SELECT * FROM network_scan.flattened_service_cpes WHERE service_id=service_row.service_id
-                        LOOP
-                            cpes_array := array_append(cpes_array, service_cpes_row.cpe::json);
-                        END LOOP;
-
-                        service_json := json_build_object(
-                            'name', service_row.name,
-                            'confidence', service_row.confidence,
-                            'method', service_row.method,
-                            'version', service_row.version,
-                            'product', service_row.product,
-                            'extrainfo', service_row.extrainfo,
-                            'tunnel', service_row.tunnel,
-                            'proto', service_row.proto,
-                            'rpcnum', service_row.rpcnum,
-                            'lowver', service_row.lowver,
-                            'highver', service_row.highver,
-                            'hostname', service_row.hostname,
-                            'ostype', service_row.ostype,
-                            'devicetype', service_row.devicetype,
-                            'servicefp', service_row.servicefp,
-                            'cpes', cpes_array
-                        );
-                    END IF;
-
-                    -- Now we handle the script output
-                    FOR script_output_row IN SELECT * FROM network_scan.flattened_script_output WHERE host_port_id=port_row.host_port_id
-                    LOOP
-                        script_output_array := array_append(script_output_array, json_build_object(
-                            'script_name', script_output_row.script_name,
-                            'output', script_output_row.output,
-                            'elements', script_output_row.elements
-                        ));
-                    END LOOP;
-
-                    -- If we have service information, we need to grab that and append it
-                    port_json_array := array_append(port_json_array, json_build_object(
-                        'portid', port_row.portid,
-                        'protocol', port_row.protocol,
-                        'state', port_row.state,
-                        'reason', port_row.reason,
-                        'reason_ttl', port_row.reason_ttl,
-                        'service', service_json,
-                        'script_output', script_output_array
-                    ));
-                END;
-            END LOOP;
-
-            -- Now we need to sort the OS detection voodoo
-            FOR osmatch_row IN SELECT * FROM network_scan.flattened_host_osmatches WHERE host_id=host_address.host_id
-            LOOP
-                DECLARE
-                    osclasses_json_array json[];
-                    cpes_array json[];
-                BEGIN
-                    -- For some scans, we won't have osmatch information
-                    IF NOT FOUND THEN
-                        osmatches_json_array := NULL;
-                        EXIT;
-                    END IF;
-
-                    -- OSClasses exist under osmatches (and ALSO have CPEs)
-                    FOR osclass_row IN SELECT * FROM network_scan.flattened_host_osmatches_osclasses WHERE host_osmatch_id=osmatch_row.host_osmatch_id
-                    LOOP
-                        -- Grabs the CPEs
-                        FOR osclass_cpe_row IN SELECT * FROM network_scan.flattened_osclass_cpes WHERE osclass_id=osclass_row.osclass_id
-                        LOOP
-                            cpes_array := array_append(cpes_array, osclass_cpe_row.cpe::json);
-                        END LOOP;
-
-                        osclasses_json_array := array_append(osclasses_json_array, json_build_object(
-                            'accuracy', osclass_row.accuracy,
-                            'vendor', osclass_row.vendor,
-                            'osgen', osclass_row.osgen,
-                            'ostype', osclass_row.ostype,
-                            'osfamily', osclass_row.osfamily,
-                            'cpes', cpes_array
-                        ));
-
-                    END LOOP;
-                    osmatches_json_array := array_append(osmatches_json_array, json_build_object(
-                        'name', osmatch_row.name,
-                        'accuracy', osmatch_row.accuracy,
-                        'osclasses', osclasses_json_array
-                    ));
-                END;
-            END LOOP;
-
-            -- Now that we have all the bits, assembly the host object
-            host_json_array := array_append(host_json_array, json_build_object(
-                'addr', host_address.ip_address,
-                'mac_address', host_address.mac_address,
-                'vendor', host_address.vendor,
-                'state', host_address.state,
-                'reason', host_address.reason,
-                'reason_ttl', host_address.reason_ttl,
-                'hostname', hostname_json_array,
-                'ports', port_json_array,
-                'osmatches', osmatches_json_array
-            ));
-        END;
+        host_json_array := array_append(
+            host_json_array, network_scan.export_host(current_host_id)
+        );
     END LOOP;
 
     -- Final bit get the scan_type back out
-    SELECT * INTO scan_row FROM network_scan.scans WHERE id=_scan_id;
-
     scan_json := json_build_object(
         'hosts', host_json_array,
         'scan_type', scan_row.scan_type,

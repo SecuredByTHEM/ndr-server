bede3e492139b956681b71a95f003c2d83d9cc79
Michael Casadevall <michaelc@them.com>
2017-06-29 19:00:44 -0400

    Update MAC information if we get a new vendor string along the way
    Signed-off-by: Michael Casadevall <michaelc@them.com>

--- sql/functions/network_scan/R__get_or_create_mac_address.sql
+++ sql/functions/network_scan/R__get_or_create_mac_address.sql
@@ -6,11 +6,18 @@ CREATE OR REPLACE FUNCTION network_scan.get_or_create_mac_address(_mac_address m
     AS $$
 DECLARE
     mac_addr_id bigint;
+    vendor_str text;
 BEGIN
-    SELECT id INTO mac_addr_id FROM network_scan.mac_addresses WHERE mac_address=_mac_address;
+    SELECT id, vendor INTO mac_addr_id, vendor_str FROM network_scan.mac_addresses WHERE mac_address=_mac_address;
     IF NOT FOUND THEN
         -- MACs are dynamically added as they're seen across scans
         INSERT INTO network_scan.mac_addresses(mac_address, vendor) VALUES (_mac_address, _vendor) RETURNING id INTO mac_addr_id;
+        RETURN mac_addr_id;
+    END IF;
+
+    -- If the vendor is NOT NULL, and the vendor in the table is, we'll update it
+    IF _vendor IS NOT NULL AND vendor_str != _vendor THEN
+        UPDATE network_scan.mac_addresses SET vendor=_vendor WHERE id=mac_addr_id;
     END IF;
 
     RETURN mac_addr_id;
